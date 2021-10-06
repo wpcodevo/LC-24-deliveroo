bae12bc0ddac7a8bd6cf5fb54fbf15095a0a923f
wpcodevo <ziddah.edem@gmail.com>
2021-10-06 21:31:13 +0000

    finished the design with no animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,61 @@
-console.log("hello");
+/* =========== Show Navbar =========== */
+const navbar = document.querySelector(".navbar");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+});
+
+/* =========== Scroll Position =========== */
+
+const header = document.querySelector(".header");
+const scrollLink = document.querySelectorAll(".navbar a:not(:last-child)");
+
+// Smooth Scroll
+Array.from(scrollLink).map((link) => {
+  link.addEventListener("click", (e) => {
+    // Prevent Default
+    e.preventDefault();
+
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    let position = element.offsetTop - 90;
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: "smooth",
+    });
+    navbar.classList.remove("show");
+  });
+});
+
+/* =========== Preloader =========== */
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    preloader.style.display = "none";
+  }, 2000);
+});
+
+/* =========== Scroll Top =========== */
+const scrollTop = document.querySelector(".scroll-top");
+
+scrollTop.addEventListener("click", () => {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("scroll", (e) => {
+  const scrollHeight = window.pageYOffset;
+
+  if (scrollHeight > 300) {
+    scrollTop.classList.add("show");
+  } else {
+    scrollTop.classList.remove("show");
+  }
+});
